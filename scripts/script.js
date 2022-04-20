ae84ecfbd29694b2e65ea3d293835b2ad32732b9
Kristina Epifanova <cristinaepifanova@gmail.com>
2022-04-20 23:26:36 +0300

    fixed

--- scripts/script.js
+++ scripts/script.js
@@ -5,19 +5,20 @@ const saveButton = document.querySelector('.form__submit');
 
 
 function popupOpen() {
-    popup.classList.toggle('popup_opened');
+    popup.classList.add('popup_opened');
     let name = document.querySelector('.profile__name').textContent;
     let job = document.querySelector('.profile__job').textContent;
     popup.querySelector('.form__input_type_name').value = name;
     popup.querySelector('.form__input_type_job').value = job;
 }
 editButton.addEventListener('click', popupOpen)
-closeButton.addEventListener('click', popupOpen)
 
-function popupSave() {
-    popup.classList.toggle('popup_opened');
+function popupClose(){
+    popup.classList.remove('popup_opened')
 }
-saveButton.addEventListener('click', popupSave)
+closeButton.addEventListener('click', popupClose)
+
+
 
 let form = popup.querySelector('.form_type_profile');
 let inputName = form.querySelector('.form__input_type_name');
@@ -25,15 +26,13 @@ let inputJob = form.querySelector('.form__input_type_job');
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
+
     document.querySelector('.profile__name').textContent = inputName.value;
     document.querySelector('.profile__job').textContent = inputJob.value;
+    popup.classList.remove('popup_opened');
 }
 form.addEventListener('submit', formSubmitHandler); 
 
-let like=document.querySelector('.photo-grid__like');
-like.addEventListener('click', function(){
-   
-    like.classList.toggle('photo-grid__like_active')
-})
+
 
 
